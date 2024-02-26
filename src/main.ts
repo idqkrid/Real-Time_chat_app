3036a16605319a286467b6d966db8971103f0b94
ParkDongGyu <idqkrid@naver.com>
2024-02-26 20:03:18 +0900

    'second_backend_finsh'

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { APP_FILTER, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
+// import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress';
+
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { graphqlUploadExpress } from 'graphql-upload';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'http://localhost:5173',
